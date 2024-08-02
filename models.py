0e5ce61bca44eff1aeff6e4b79d3292367a5d5f7
sachin-source <sachinmgvt@gmail.com>
2024-08-02 19:05:53 +0530

    add question schema

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+class Questions(Base):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(String, index=True)
