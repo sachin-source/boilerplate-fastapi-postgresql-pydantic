cace9a00db65e823416891e0e250e398f1226877
sachin-source <sachinmgvt@gmail.com>
2024-08-02 19:28:35 +0530

    Add choice schema

--- models.py
+++ models.py
@@ -6,3 +6,11 @@ class Questions(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     question_text = Column(String, index=True)
+
+class Choices(Base):
+    __tablename__ = 'choices'
+
+    id = Column(Integer, primary_key=True, index=True)
+    choice_text = Column(String, index=True)
+    is_correct = Column(Boolean, default=False)
+    question_id = Column(Integer, ForeignKey("questions.id"))
