29d83c3b94bf0e5717294fc441dfe4aa39bcfb57
sachin-source <sachinmgvt@gmail.com>
2024-08-02 19:58:07 +0530

    changed postgres password

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:test1234!@localhost:5432/QuizApplicationYT'
+URL_DATABASE = 'postgresql://postgres:postgres@localhost:5432/QuizApplicationYT'
 
 engine = create_engine(URL_DATABASE)
 
