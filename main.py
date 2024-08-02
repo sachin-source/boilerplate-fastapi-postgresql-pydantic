db710890894d898d35301c0d674bcf1369b5c519
sachin-source <sachinmgvt@gmail.com>
2024-08-02 19:41:31 +0530

    post method to store question

--- main.py
+++ main.py
@@ -23,3 +23,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/questions/")
+async def create_questions(question: QuestionBase, db: db_dependency):
+    db_question = models.Questions(question_text = question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db.add(db_choice)
+    db.commit()
