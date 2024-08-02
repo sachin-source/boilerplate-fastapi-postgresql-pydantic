9f118a9940bd15fbf5a4b8cd1cc429b4df7f7bd7
sachin-source <sachinmgvt@gmail.com>
2024-08-02 19:32:54 +0530

    database connection method

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
 
 class ChoiceBase(BaseModel):
     choice_text: str
@@ -11,3 +16,10 @@ class ChoiceBase(BaseModel):
 class QuestionBase(BaseModel):
     question_text: str
     choices: List[ChoiceBase]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
